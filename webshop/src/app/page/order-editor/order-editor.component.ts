47441bf62be13a5ba1ae883f13bec72f5cc78f84
GergelyPorvai <porvai.gergely@gmail.com>
2022-12-05 14:26:16 +0100

    editor v1

--- webshop/src/app/page/order-editor/order-editor.component.ts
+++ webshop/src/app/page/order-editor/order-editor.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { Order } from 'src/app/model/order';
+import { OrderService } from 'src/app/service/order.service';
 
 @Component({
   selector: 'app-order-editor',
   templateUrl: './order-editor.component.html',
-  styleUrls: ['./order-editor.component.scss']
+  styleUrls: ['./order-editor.component.scss'],
 })
 export class OrderEditorComponent {
-
+  order$: Observable<Order> = this.ar.params.pipe(
+    switchMap((params) => this.orderService.get(params['id']))
+  );
+  constructor(
+    private ar: ActivatedRoute,
+    private orderService: OrderService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {}
 }
