ab23acec5c10439d19ac5982ba260df5f0366691
balintpecz <47353150+balintpecz@users.noreply.github.com>
2022-12-13 16:29:29 +0100

    Delete material-dashboard-angular2-master directory

--- material-dashboard-angular2-master/src/app/model/column.ts
+++
@@ -1,8 +0,0 @@
-export class Column {
-    index:number=0;
-    name:string = '';
-    title:string = '';
-    type:string='';
-    sortDir:string='none';
-    footer:boolean=false;
-  }
--- material-dashboard-angular2-master/src/app/model/order.ts
+++
@@ -1,9 +0,0 @@
-export class Order {
-    id: number = 0;
-    customerID: number = 0;
-    productID: number = 0;
-    amount: number = 0;
-    status: string = '';
-
-    constructor() { }
-}
